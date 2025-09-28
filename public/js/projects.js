878895d9896c045371b607f5ba2fddef3958f4cd
Aldo Rosales Benhumea <127135506+AldoRoss@users.noreply.github.com>
2025-09-28 15:24:51 -0600

    Responisive mobile

--- public/js/projects.js
+++ public/js/projects.js
@@ -1,36 +1,8 @@
-document.addEventListener('DOMContentLoaded', () => {
-    console.log("DOM cargado"); // Verificar que el script está cargado
+document.addEventListener('DOMContentLoaded', function() {
+    const hamburgerBtn = document.getElementById('hamburger-menu-btn');
+    const navLinks = document.getElementById('nav-links');
 
-    const audio = document.getElementById('screenSound');
-    audio.play(); // Reproducir el sonido al cargar la pantalla
-
-    let currentIndex = parseInt(localStorage.getItem('currentIndex'));
-    let numImagenes = parseInt(localStorage.getItem('numImagenes'));
-    let retro = localStorage.getItem('retro');
-
-    if (output) output.textContent = retro
-
-    window.changeButton = function () {
-        
-        console.log(`Índice actual: ${currentIndex}, Total imágenes: ${numImagenes}`);
-
-        // Si es la última imagen, redirigir a pantalla final
-        if (currentIndex >= numImagenes) {
-            window.location.href = '/terminado_ca';
-        } else {
-            // De lo contrario, redirigir a la siguiente pantalla
-            currentIndex++;
-            if (currentIndex >= numImagenes) {
-                window.location.href = '/terminado_ca';
-            }
-            else{
-                localStorage.setItem('currentIndex', currentIndex); // Actualizar índice
-                window.location.href = '/cuentameAlgo';
-        }
-        }
-    }
-
-    window.repeat = function () {
-        window.location.href = '/cuentameAlgo'; 
-    }
+    hamburgerBtn.addEventListener('click', function() {
+        navLinks.classList.toggle('active');
+    });
 });
