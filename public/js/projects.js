16087154598bd7b458b2c14149e057c71a5fbe6e
Aldo Rosales Benhumea <127135506+AldoRoss@users.noreply.github.com>
2025-09-28 21:33:42 -0600

    carrusel

--- public/js/projects.js
+++ public/js/projects.js
@@ -1,8 +1,25 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const hamburgerBtn = document.getElementById('hamburger-menu-btn');
-    const navLinks = document.getElementById('nav-links');
+const items = document.querySelectorAll('.carousel-item');
+const prevBtn = document.querySelector('.prev-btn');
+const nextBtn = document.querySelector('.next-btn');
+let currentIndex = 0;
 
-    hamburgerBtn.addEventListener('click', function() {
-        navLinks.classList.toggle('active');
-    });
+function showSlide(index) {
+    if (index < 0) {
+        currentIndex = items.length - 1;
+    } else if (index >= items.length) {
+        currentIndex = 0;
+    } else {
+        currentIndex = index;
+    }
+
+    items.forEach(item => item.classList.remove('active'));
+    items[currentIndex].classList.add('active');
+}
+
+prevBtn.addEventListener('click', () => {
+    showSlide(currentIndex - 1);
+});
+
+nextBtn.addEventListener('click', () => {
+    showSlide(currentIndex + 1);
 });
